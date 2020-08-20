053c53df95ad334c5a8f2afdc2714e4e5656d212
jiangjiudong <jiangjiudong@58.com>
2020-08-20 20:36:21 +0800

    添加

--- sqlite.c
+++ sqlite.c
@@ -35,12 +35,13 @@ typedef struct {
 } Statement;
 
 #define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+#define size_add(x, y) （x+y）
 
-const uint32_t ID_SIZE =sizeof(((Row*)0)->id);
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
 const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
 const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
 const uint32_t ID_OFFSET = 0;
-const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t USERNAME_OFFSET = size_add(ID_OFFSET,ID_SIZE);
 const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
 const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
 
