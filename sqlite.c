bec51109def8ae7a172344cadf874631d51e1961
jiangjiudong <jiangjiudong@58.com>
2020-08-20 20:50:29 +0800

    添加

--- sqlite.c
+++ sqlite.c
@@ -41,17 +41,18 @@ const uint32_t ID_SIZE = size_of_attribute(Row, id);
 const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
 const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
 const uint32_t ID_OFFSET = 0;
-const uint32_t USERNAME_OFFSET = size_add(ID_OFFSET,ID_SIZE);
-const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
-const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
 
 void serialize_row(Row* source, void* destination) {
+  uint32_t USERNAME_OFFSET = ID_OFFSET+ID_SIZE;
+  uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
   memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
   memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
   memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
 }
 
 void deserialize_row(void* source, Row* destination) {
+  uint32_t USERNAME_OFFSET = ID_OFFSET+ID_SIZE;
+  uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
   memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
   memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
   memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
