5d318e6439470448a6a9c16ed0f1318cc1b9661b
jiangjiudong <jiangjiudong@58.com>
2020-08-20 20:48:36 +0800

    添加

--- sqlite.c
+++ sqlite.c
@@ -35,28 +35,26 @@ typedef struct {
 } Statement;
 
 #define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+#define size_add(x, y) (x+y)
 
 const uint32_t ID_SIZE = size_of_attribute(Row, id);
 const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
 const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
 const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = size_add(ID_OFFSET,ID_SIZE);
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
 
 void serialize_row(Row* source, void* destination) {
-  uint32_t id_offset=0;
-  uint32_t username_offset=id_offset+ID_SIZE;
-  uint32_t email_offset=username_offset+USERNAME_SIZE;
-  memcpy(destination + id_offset, &(source->id), ID_SIZE);
-  memcpy(destination + username_offset, &(source->username), USERNAME_SIZE);
-  memcpy(destination + email_offset, &(source->email), EMAIL_SIZE);
+  memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+  memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+  memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
 }
 
 void deserialize_row(void* source, Row* destination) {
-  uint32_t id_offset=0;
-  uint32_t username_offset=id_offset+ID_SIZE;
-  uint32_t email_offset=username_offset+USERNAME_SIZE;
-  memcpy(&(destination->id), source + id_offset, ID_SIZE);
-  memcpy(&(destination->username), source + username_offset, USERNAME_SIZE);
-  memcpy(&(destination->email), source + email_offset, EMAIL_SIZE);
+  memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
+  memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
+  memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
 }
 
 void print_prompt() { printf("db > "); }
