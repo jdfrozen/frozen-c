fab053392db33a784b316ec11533267d11378a5a
jiangjiudong <jiangjiudong@58.com>
2020-08-20 20:03:34 +0800

    添加

--- sqlite.c
+++ sqlite.c
@@ -82,8 +82,7 @@ PrepareResult prepare_statement(InputBuffer* input_buffer,
   if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
     statement->type = STATEMENT_INSERT;
     int args_assigned = sscanf(
-      input_buffer->buffer,"insert %d %s %s",&(statement->row_to_insert.id)
-      statement->row_to_insert.username,statement->row_to_insert.email
+      input_buffer->buffer,"insert %d %s %s",&(statement->row_to_insert.id),statement->row_to_insert.username,statement->row_to_insert.email
     )
     if(args_assigned<3){
       return PREPARE_SYNTAX_ERROR;
