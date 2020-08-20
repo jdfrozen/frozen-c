acb1af962e7bb38001afe20b7f1df9cd78210b3f
jiangjiudong <jiangjiudong@58.com>
2020-08-20 20:07:38 +0800

    添加

--- sqlite.c
+++ sqlite.c
@@ -83,7 +83,7 @@ PrepareResult prepare_statement(InputBuffer* input_buffer,
     statement->type = STATEMENT_INSERT;
     int args_assigned = sscanf(
       input_buffer->buffer,"insert %d %s %s",&(statement->row_to_insert.id),statement->row_to_insert.username,statement->row_to_insert.email
-    )
+    );
     if(args_assigned<3){
       return PREPARE_SYNTAX_ERROR;
     }
