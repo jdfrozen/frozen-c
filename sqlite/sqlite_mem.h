99b09da14b06a8b29c40f38be0c0bf7909a30098
jdfrozen <616769416@qq.com>
2020-08-27 18:39:37 +0800

    添加

--- sqlite/sqlite_mem.h
+++ sqlite/sqlite_mem.h
@@ -1,9 +1,14 @@
-typedef enum { PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT } PrepareResult;
-typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
-typedef enum { EXECUTE_SUCCESS, EXECUTE_TABLE_FULL } ExecuteResult;
 //行定义
 #define COLUMN_USERNAME_SIZE 32
 #define COLUMN_EMAIL_SIZE 255
+#define TABLE_MAX_PAGES 100
+//获取结构体属性内存
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+
+typedef enum { PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT } PrepareResult;
+typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
+typedef enum { EXECUTE_SUCCESS, EXECUTE_TABLE_FULL } ExecuteResult;
+
 typedef struct {
   uint32_t id;
   char username[COLUMN_USERNAME_SIZE + 1];
@@ -14,25 +19,20 @@ typedef struct {
   StatementType type;
   Row row_to_insert;
 }Statement;
-
-//行与地址存储转换
-#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
-#define size_add(x, y) (x+y)
-
-#define TABLE_MAX_PAGES 100
+//页定义
 typedef struct {
   int file_descriptor;
   uint32_t file_length;
   uint32_t num_pages;
   void* pages[TABLE_MAX_PAGES];
 } Pager;
-
+//表定义
 typedef struct {
   void* pages[TABLE_MAX_PAGES];
   Pager* pager;
   uint32_t root_page_num;
 } Table;
-
+//游标定义
 typedef struct {
   Table* table;
   uint32_t page_num;
@@ -45,8 +45,10 @@ ExecuteResult execute_statement(Statement* statement,Table* table);
 ExecuteResult execute_insert(Statement* statement, Table* table);
 //select
 ExecuteResult execute_select(Statement* statement, Table* table);
+//打开表
 Table* db_open(const char* filename);
-void* get_page(Pager* pager, uint32_t page_num);
+//关闭表
 void db_close(Table* table);
+void* get_page(Pager* pager, uint32_t page_num);
 void print_leaf_node(void* node);
 void print_constants();
