e08ba2ee2472c24f0432bc0f99db1299ef7ee843
jdfrozen <616769416@qq.com>
2020-10-09 12:13:09 +0800

    文件状态

--- signal/signal.c
+++ signal/signal.c
@@ -5,25 +5,25 @@ void sigroutine(int dunno){
     /* 信号处理例程，其中dunno将会得到信号的值 */
         switch (dunno) {
         case 1:
-        printf("Get a signal -- SIGHUP ");
-        fflush(stdin);
+        printf("Get a signal -- SIGHUP \n");
         break;
         case 2:
-        printf("Get a signal -- SIGINT ");
-        fflush(stdin);
+        printf("Get a signal -- SIGINT \n");
         break;
         case 3:
-        printf("Get a signal -- SIGQUIT ");
-        fflush(stdin);
+        printf("Get a signal -- SIGQUIT \n");
         break;
         }
         return;
 }
 int main() {
-        printf("process id is %d ",getpid());
-        fflush(stdin);
+        printf("process id is %d \n",getpid());
         signal(SIGHUP, sigroutine);
         signal(SIGINT, sigroutine);
         signal(SIGQUIT, sigroutine);
-        sleep(60);
+        for(;;){
+            printf("sleep 1 \n");
+            sleep(1);
+        }
+
 }
