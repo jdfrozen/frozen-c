3285f616e44ca7fceeabdf3c8a159959382196a6
jdfrozen <616769416@qq.com>
2020-12-01 12:45:35 +0800

    linux 多线程编程

--- concurent/Thread.c
+++ concurent/Thread.c
@@ -1,8 +1,23 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
 #include<pthread.h>
 
-int main(int argc ,char *argv[]){
+void run(){
+    printf("this is a pthread. \n");
+    sleep(30);
+}
 
+int main(int argc ,char *argv[]){
+    pthread_t id;
+    int i,ret;
+    ret=pthread_create(&id,NULL,(void *)thread,NULL);
+    if(ret!=0){
+        printf("Create pthread error!\n");
+        exit(1);
+    }
+    printf("this is the main process. \n");
+    pthread_join(id,NULL);
     return 0;
 }
 
