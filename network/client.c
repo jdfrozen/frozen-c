976ce78194f4ca7f5ce1beb24c419a3259aed344
jdfrozen <616769416@qq.com>
2020-12-06 21:31:49 +0800

    添加网络实现

--- network/client.c
+++ network/client.c
@@ -4,44 +4,45 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+
 #define BUF_SIZE 1024
-void error_handling(char *message);
-int main(int argc, char* argv[]){
-    struct sockaddr_in serv_addr;
-    char message[BUF_SIZE];
-    int str_len;
-    if(argc!=3){
+void error_handling(char * messages);
+
+int main(int argc, char *argv[]){
+    if(argc != 3){
         printf("Usage : %s <IP> <port>\n", argv[0]);
         exit(1);
     }
-    int sock=socket(PF_INET, SOCK_STREAM, 0);
+    int sock = socket(PF_INET, SOCK_DGRAM, 0);
     if(sock == -1){error_handling("socket() error");}
+    struct sockaddr_in serv_addr, from_addr;
     memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family=AF_INET;
-    serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
-    serv_addr.sin_port=htons(atoi(argv[2]));
-    if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1){error_handling("connect() error!");}
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_addr.sin_port = htons(atoi(argv[2]));
+    //也可以基于“连接”
+    //connect(sock,(struct sockaddr*)&serv_addr,sizeof(serv_addr));
+    char message[BUF_SIZE];
+    int strLen;
+    socklen_t addr_size;
     while(1){
-        fputs("Input message(Q to quit):",stdout);
-        fgets(message,BUF_SIZE,stdin);
-        if(!strcmp(message,"q\n")||!strcmp(message,"Q\n")){break;}
-        str_len=write(sock,message,strlen(message));
-        int recv_len=0;
-        while(recv_len<str_len){
-            int recv_cnt=read(sock, &message[recv_len], BUF_SIZE-1);
-            if(recv_cnt==-1){
-                error_handling("read() error!");
-            }
-            recv_len+=recv_cnt;
-        }
-        message[str_len]=0;
-        printf("Message from server: %s \n", message);
+        puts("Input message(Q to quit):");
+        fgets(message, BUF_SIZE, stdin);
+        if(!strcmp(message, "q\n") || !strcmp(message, "Q\n")){break;}
+        sendto(sock, message, strlen(message), 0, (struct sockaddr*)&serv_addr,sizeof(serv_addr));
+        //基于“连接”，可以使用IO函数write,read
+        //write(sock, message, strlen(message));
+        addr_size = sizeof(from_addr);
+        strLen = recvfrom(sock, message, BUF_SIZE, 0, (struct sockaddr*)&from_addr, &addr_size);
+        //strLen = read(sock,message,sizeof(message)-1);
+        message[strLen]=0;
+        printf("Message from server: %s\n", message);
     }
     close(sock);
     return 0;
 }
-void error_handling(char *message){
-    fputs(message, stderr);
-    fputc('\n', stderr);
+
+void error_handling(char * messages){
+    puts(messages);
     exit(1);
 }
--- network/server.c
+++ network/server.c
@@ -4,47 +4,40 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-
 #define BUF_SIZE 1024
-void error_handling(char *message);
+void error_handling(char * messages);
 int main(int argc, char *argv[]){
-    int serv_sock;
-    int clnt_sock;
-    struct sockaddr_in serv_addr;
-    struct sockaddr_in clnt_addr;
-    socklen_t clnt_addr_size;
-    char message[BUF_SIZE];
-    if(argc!=2){
+    if(argc != 2){
         printf("Usage : %s <port>\n", argv[0]);
         exit(1);
     }
-    serv_sock=socket(PF_INET, SOCK_STREAM, 0);
-    if(serv_sock == -1){error_handling("socket() error");}
-    memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family=AF_INET;
-    serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);
-    serv_addr.sin_port=htons(atoi(argv[1]));
-    if(bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr))==-1 ){error_handling("bind() error");}
-    if(listen(serv_sock, 5)==-1){error_handling("listen() error");}
-    clnt_addr_size=sizeof(clnt_addr);
-    int str_len;
-    for(int i=0;i<5;i++){
-        clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_addr,&clnt_addr_size);
-        if(clnt_sock==-1){
-            error_handling("accept() error");
-        }else{
-            printf("Connect client %d\n",i+1);
-        }
-        while((str_len=read(clnt_sock,message,BUF_SIZE))!=0){
-            write(clnt_sock, message,str_len);
-        }
-        close(clnt_sock);
+    //format addr
+    int serverSock =socket(PF_INET, SOCK_DGRAM, 0);
+    if(serverSock == -1){error_handling("socket() error");}
+    struct sockaddr_in serverAddr, clientAddr;
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serverAddr.sin_port = htons(atoi(argv[1]));
+    //bind
+    if(bind(serverSock, (struct sockaddr*) &serverAddr, sizeof(serverAddr)) == -1)
+        error_handling("bind() error");
+    puts("Server start...");
+    //data
+    char message[BUF_SIZE];
+    int strLen;
+    socklen_t clientAddrSize;
+    while(1){
+        clientAddrSize = sizeof(clientAddr);
+        strLen = recvfrom(serverSock, message, BUF_SIZE, 0, (struct sockaddr*) &clientAddr, &clientAddrSize);
+        sendto(serverSock, message, strLen, 0, (struct sockaddr*)&clientAddr, clientAddrSize);
     }
-    close(serv_sock);
+    close(serverSock);
+    puts("Server close...");
     return 0;
 }
-void error_handling(char *message){
-    fputs(message, stderr);
-    fputc('\n', stderr);
+void error_handling(char * messages)
+{
+    puts(messages);
     exit(1);
 }
