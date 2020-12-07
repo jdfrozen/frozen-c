d2804476b00e323a4a9ced12d6de8301eeced902
jdfrozen <616769416@qq.com>
2020-12-07 15:23:35 +0800

    去掉空格

--- network/client.c
+++ network/client.c
@@ -1,47 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <sys/socket.h>
-#define BUF_SIZE 1024
+#include <netdb.h>
 void error_handling(char *message);
-int main(int argc, char* argv[]){
-    struct sockaddr_in serv_addr;
-    char message[BUF_SIZE];
-    int str_len;
-    if(argc!=3){
-        printf("Usage : %s <IP> <port>\n", argv[0]);
+int main(int argc, char *argv[]){
+    int i;
+    struct hostent *host;
+    if (argc != 2){
+        printf("Usage:%s<addr>\n", argv[0]);
         exit(1);
     }
-    int sock=socket(PF_INET, SOCK_STREAM, 0);
-    if(sock == -1){error_handling("socket() error");}
-    memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family=AF_INET;
-    serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
-    serv_addr.sin_port=htons(atoi(argv[2]));
-    if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1){error_handling("connect() error!");}
-
-    fputs("Input message(Q to quit):",stdout);
-    fgets(message,BUF_SIZE,stdin);
-    //echo一次
-    str_len=write(sock,message,strlen(message));
-    int recv_len=0;
-    while(recv_len<str_len){
-        int recv_cnt=read(sock, &message[recv_len], BUF_SIZE-1);
-        if(recv_cnt==-1){
-            error_handling("read() error!");
-        }
-        recv_len+=recv_cnt;
-    }
-    message[str_len]=0;
-    printf("Message from server: %s \n", message);
-    //测试服务端只关闭它的输出流，输入流还能接收数据
-    write(sock,"Thank you",10);
-    close(sock);
+    host = gethostbyname(argv[1]);
+    if (!host)error_handling("gethost... error");
+    printf("Official name:%s\n", host->h_name);
+    for (i = 0; host->h_aliases[i]; i++)
+        printf("Aliases %d:%s\n", i + 1, host->h_aliases[i]);
+    printf("Address type:%s\n", (host->h_addrtype == AF_INET) ? "AF_INET" : "AF_INET6");
+    for (i = 0; host->h_addr_list[i]; i++)
+        printf("IP addr %d:%s\n", i + 1, inet_ntoa(*(struct in_addr *)host->h_addr_list[i]));
     return 0;
 }
-void error_handling(char *message){
+void error_handling(char *message)
+{
     fputs(message, stderr);
     fputc('\n', stderr);
     exit(1);
--- network/server.c
+++ network/server.c
@@ -3,49 +3,34 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <sys/socket.h>
-
-#define BUF_SIZE 1024
+#include <netdb.h>
 void error_handling(char *message);
-int main(int argc, char *argv[]){
-    int serv_sock;
-    int clnt_sock;
-    struct sockaddr_in serv_addr;
-    struct sockaddr_in clnt_addr;
-    socklen_t clnt_addr_size;
-    char message[BUF_SIZE];
-    if(argc!=2){
-        printf("Usage : %s <port>\n", argv[0]);
+
+int main(int argc, char *argv[])
+{
+    int i;
+    struct hostent *host;
+    struct sockaddr_in addr;
+    if (argc != 2){
+        printf("Usage : %s <IP>\n", argv[0]);
         exit(1);
     }
-    serv_sock=socket(PF_INET, SOCK_STREAM, 0);
-    if(serv_sock == -1){error_handling("socket() error");}
-    memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family=AF_INET;
-    serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);
-    serv_addr.sin_port=htons(atoi(argv[1]));
-    if(bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr))==-1 ){error_handling("bind() error");}
-    if(listen(serv_sock, 5)==-1){error_handling("listen() error");}
-    clnt_addr_size=sizeof(clnt_addr);
-    int str_len;
-    clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_addr,&clnt_addr_size);
-    if(clnt_sock==-1){
-        error_handling("accept() error");
-    }else{
-        printf("Connect client \n");
-    }
-    //echo一次
-    read(clnt_sock,message,BUF_SIZE);
-    write(clnt_sock, message,str_len);
-    printf("message is %s \n",message);
-    shutdown(clnt_sock,SHUT_WR);
-    read(clnt_sock,message,BUF_SIZE);
-    printf("message last is %s \n",message);
-    close(clnt_sock);
-    close(serv_sock);
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_addr.s_addr = inet_addr(argv[1]);
+    host = gethostbyaddr((char *)&addr.sin_addr, 4, AF_INET);
+    if (!host)error_handling("gethost... error");
+    printf("Official name: %s \n", host->h_name);
+    for (i = 0; host->h_aliases[i]; i++)
+        printf("Aliases %d: %s \n", i + 1, host->h_aliases[i]);
+
+    printf("Address type: %s \n",(host->h_addrtype == AF_INET) ? "AF_INET" : "AF_INET6");
+    for (i = 0; host->h_addr_list[i]; i++)
+        printf("IP addr %d: %s \n", i + 1,inet_ntoa(*(struct in_addr *)host->h_addr_list[i]));
     return 0;
 }
-void error_handling(char *message){
+
+void error_handling(char *message)
+{
     fputs(message, stderr);
     fputc('\n', stderr);
     exit(1);
