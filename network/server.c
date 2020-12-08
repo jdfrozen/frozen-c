c012e8e697138099ed81fc8ea888c2acb324cb2c
jdfrozen <616769416@qq.com>
2020-12-08 12:39:09 +0800

    去掉空格

--- network/server.c
+++ network/server.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 int main(int argc,char *argv[]){
+    int status;
     pid_t pid = fork();
     if(pid==0){
         puts("Hi,I am a child process! ");
+        return 3;
     }else{
         printf("Child Proc: %d \n",pid);
-        sleep(30);
-    }
-    if(pid==0){
-        puts("End child process! ");
-    }else{
-        puts("End parent process! ");
+        if(pid==0){
+            exit(7);
+        }else{
+            printf("Child PID: %d \n",pid);
+            wait(&status);
+            if(){printf("Child send one: %d \n",WEXITSTATUS(status));}
+            wait(&status);
+            if(){printf("Child send two: %d \n",WEXITSTATUS(status));}
+            sleep(30);
+        }
     }
     return 0;
 }
