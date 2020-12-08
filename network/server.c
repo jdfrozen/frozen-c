ad9c8020cebe3f4136d861ace1fdf2bddab8f6ee
jdfrozen <616769416@qq.com>
2020-12-08 12:40:13 +0800

    去掉空格

--- network/server.c
+++ network/server.c
@@ -15,9 +15,9 @@ int main(int argc,char *argv[]){
         }else{
             printf("Child PID: %d \n",pid);
             wait(&status);
-            if(){printf("Child send one: %d \n",WEXITSTATUS(status));}
+            if(WIFEXITED(status)){printf("Child send one: %d \n",WEXITSTATUS(status));}
             wait(&status);
-            if(){printf("Child send two: %d \n",WEXITSTATUS(status));}
+            if(WIFEXITED(status)){printf("Child send two: %d \n",WEXITSTATUS(status));}
             sleep(30);
         }
     }
