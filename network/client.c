d7b8bbb99273f0b35a82bd188b2549fef561f93b
jdfrozen <616769416@qq.com>
2020-12-09 18:03:33 +0800

    去掉空格

--- network/client.c
+++ network/client.c
@@ -1,59 +1,29 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<unistd.h>
-#include<arpa/inet.h>
-#include<sys/socket.h>
-
-#define BUF_SIZE 30
-
-void error_handling(char* message);
-void read_routine(int sock, char* buf);
-void write_routine(int sock, char* buf);
-
-int main(int argc, char* argv[]) {
-    struct sockaddr_in serv_addr;
-    char buf[BUF_SIZE];
-    if (argc != 3) {
-        printf("Usage: %s <IP> <port>\n", argv[0]);
-        exit(1);
-    }
-    int sock = socket(PF_INET, SOCK_STREAM, 0);
-    memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
-    serv_addr.sin_port = htons(atoi(argv[2]));
-
-    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1){error_handling("connect() error!");}
-    pid_t pid = fork();
-    if (pid == 0){
-        write_routine(sock, buf);
-    }else{
-        read_routine(sock, buf);
-    }
-    close(sock);
-    return 0;
-}
-void error_handling(char* message) {
-    fputs(message, stderr);
-    fputc('\n', stderr);
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+void error_handling(char * messages);
+int main(int argc,char *argv[]){
+  int sock;
+  struct sockaddr_in send_adr;
+  if (argc != 3){
+    printf("Usage: %s <IP> <port> \n",argv[0]);
     exit(1);
+  }
+  sock = socket(AF_INET,SOCK_STREAM,0);
+  memset(&send_adr,0,sizeof(send_adr));
+  send_adr.sin_family = AF_INET;
+  send_adr.sin_addr.s_addr = inet_addr(argv[1]);
+  send_adr.sin_port = htons(atoi(argv[2]));
+  if (connect(sock,(struct sockaddr*)&send_adr,sizeof(send_adr)) == -1){error_handling("connect() error");}
+  write(sock,"1234",strlen("1234"));
+  close(sock);
+  return 0;
 }
-void read_routine(int sock, char* buf) {
-    while (1) {
-        int str_len = read(sock, buf, BUF_SIZE);
-        if (str_len == 0){return;}
-        buf[str_len] = 0;
-        printf("Message form server: %s \n", buf);
-    }
-}
-void write_routine(int sock, char* buf) {
-    while (1) {
-        fgets(buf, BUF_SIZE, stdin);
-        if (!strcmp(buf, "q\n") || !strcmp(buf, "Q\n")) {
-            shutdown(sock, SHUT_WR);
-            return;
-        }
-        write(sock, buf, strlen(buf));
-    }
+void error_handling(char * messages){
+    puts(messages);
+    exit(1);
 }
--- network/server.c
+++ network/server.c
@@ -1,72 +1,53 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<unistd.h>
-#include<arpa/inet.h>
-#include<sys/socket.h>
-#include<sys/time.h>
-#include<sys/select.h>
-
-#define BUF_SIZE 100
-
-void error_handling(char* message) {
-    fputs(message, stderr);
-    fputc('\n', stderr);
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#define BUF_SIZE 30
+void error_handling(char * messages);
+int main(int argc,char *argv[])
+{
+  int acpt_sock,recv_sock;
+  struct sockaddr_in acpt_adr,recv_adr;
+  int str_len,state;
+  socklen_t recv_adr_sz;
+  char buf[BUF_SIZE];
+  if (argc != 2){
+    printf("Usage : %s <port>\n",argv[0]);
     exit(1);
-}
+  }
 
-int main(int argc, char* argv[]) {
-    int serv_sock, clnt_sock;
-    struct sockaddr_in serv_addr, clnt_addr;
-    char buf[BUF_SIZE];
-    struct timeval timeout;
-    fd_set reads, cpy_reads;
-    if (argc != 2) {
-        printf("Usage : %s <port> \n", argv[0]);
-        exit(1);
-    }
-    serv_sock = socket(PF_INET, SOCK_STREAM, 0);
-    if (serv_sock == -1){error_handling("socket() error");}
-    memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_addr.sin_port = htons(atoi(argv[1]));
-    if (bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1){error_handling("bind() error");}
-    if (listen(serv_sock, 5) == -1){error_handling("listen() error");}
-    FD_ZERO(&reads);
-    FD_SET(serv_sock, &reads);
-    int fd_max = serv_sock;
-    while (1) {
-        cpy_reads = reads;
-        timeout.tv_sec = 5;
-        timeout.tv_usec = 5000;
-        int fd_num;
-        if ((fd_num = select(fd_max + 1, &cpy_reads, 0, 0, &timeout)) == -1){break;}
-        if (fd_num == 0){continue;}
-        for (int i = 0; i < fd_max + 1; i++) {
-            if (FD_ISSET(i, &cpy_reads)) {
-                // 判断是新的链接请求还是读数据的到来
-                if (i == serv_sock){
-                    socklen_t clnt_addr_size = sizeof(clnt_addr);
-                    clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
-                    FD_SET(clnt_sock, &reads);
-                    if (fd_max < clnt_sock) {
-                        fd_max = clnt_sock;
-                    }
-                    printf("Connected client: %d \n", clnt_sock);
-                }else {
-                    int str_len = read(i, buf, BUF_SIZE);
-                    if (str_len == 0) {
-                        FD_CLR(i, &reads);
-                        close(i);
-                        printf("closed client: %d \n", i);
-                    }else {
-                        write(i, buf, str_len);
-                    }
-                }
-            }
-        }
-    }
-    close(serv_sock);
-    return 0;
+  //绑定服务端套接字
+  acpt_sock = socket(AF_INET,SOCK_STREAM,0);
+  memset(&acpt_adr,0,sizeof(acpt_adr));
+  acpt_adr.sin_family = AF_INET;
+  acpt_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+  acpt_adr.sin_port = htons(atoi(argv[1]));
+
+  if (bind(acpt_sock,(struct sockaddr*)&acpt_adr,sizeof(acpt_adr)) == -1){error_handling("bind() error");}
+
+  listen(acpt_sock,5);
+  recv_adr_sz = sizeof(recv_adr);
+  recv_sock = accept(acpt_sock,(struct sockaddr*)&recv_adr,&recv_adr_sz);
+
+  //以非阻塞的方式验证待读入的数据是否存在,不存在则一直询问?,存在则退出
+  while(1){
+    str_len = recv(recv_sock,buf,sizeof(buf)-1,MSG_PEEK | MSG_DONTWAIT);
+    if (str_len > 0)
+      break;
+  }
+  buf[str_len] = 0;
+  printf("Buffering %d bytes: %s \n",str_len,buf);//拿到缓冲区的数据,
+  str_len = recv(recv_sock,buf,sizeof(buf)-1,0);//读入后会删除缓冲区
+  buf[str_len] = 0;
+  printf("Read again: %s \n",buf);
+  close(acpt_sock);
+  close(recv_sock);
+  return 0;
+}
+void error_handling(char * messages){
+    puts(messages);
+    exit(1);
 }
