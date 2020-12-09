88819b5c61cb5cfb00ceb0bc68db525555606c9b
jdfrozen <616769416@qq.com>
2020-12-09 13:15:20 +0800

    去掉空格

--- network/server.c
+++ network/server.c
@@ -1,25 +1,96 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
-#define BUF_SIZE 30
+#include <signal.h>
+#include <sys/wait.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
-int main(int argc,char *argv[]){
-    int fds1[2],fds2[2];
-    char str1[]="Who are you?";
-    char str2[]="Thank you for your message?";
+#define BUF_SIZE 100
+void error_handling(char *message);
+void read_childproc(int sig);
+
+int main(int argc, char *argv[]){
+    int serv_sock, clnt_sock;
+    struct sockaddr_in serv_adr, clnt_adr;
+    int fds[2];
+
+    pid_t pid;
+    struct sigaction act;
+    socklen_t adr_sz;
+    int str_len, state;
     char buf[BUF_SIZE];
-    pipe(fds1);
-    pipe(fds2);
-    pid_t pid=fork();
-    if(pid==0){
-        write(fds1[1],str1,sizeof(str1));
-        sleep(2);
-        read(fds2[0],buf,BUF_SIZE);
-        printf("Child proc output: %s \n",buf);
-    }else{
-        read(fds1[0],buf,BUF_SIZE);
-        printf("parent proc output: %s \n",buf);
-        write(fds2[1],str2,sizeof(str2));
-        sleep(3);
+    if (argc != 2){
+        printf("Usage : %s <port>\n", argv[0]);
+        exit(1);
     }
+ 	//sigaction
+    act.sa_handler = read_childproc;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = 0;
+    state = sigaction(SIGCHLD, &act, 0);]
+ 	//socket
+    serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+    //bind
+    memset(&serv_adr, 0, sizeof(serv_adr));
+    serv_adr.sin_family = AF_INET;
+    serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_adr.sin_port = htons(atoi(argv[1]));
+    if (bind(serv_sock, (struct sockaddr *)&serv_adr, sizeof(serv_adr)) == -1){error_handling("bind() error");}]
+    if (listen(serv_sock, 5) == -1){error_handling("listen() error");}
+
+    pipe(fds);
+    pid = fork();
+    //专门子进程负责保存消息
+    if (pid == 0){
+        FILE *fp = fopen("echomsg.txt", "wt");
+        char msgbuf[BUF_SIZE];
+        int i, len;
+        //接收10次消息
+        for (i = 0; i < 10; i++){
+            len = read(fds[0], msgbuf, BUF_SIZE);
+            fwrite((void *)msgbuf, 1, len, fp);
+        }
+        fclose(fp);
+        return 0;
+    }
+    while (1){
+    	//accept
+        adr_sz = sizeof(clnt_adr);
+        clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_adr, &adr_sz);
+        if (clnt_sock == -1){
+            continue;
+        }else{
+            puts("new client connected...");
+        }
+        pid = fork();
+        //子进程
+        if (pid == 0){
+            close(serv_sock);
+            while ((str_len = read(clnt_sock, buf, BUF_SIZE)) != 0){
+                write(clnt_sock, buf, str_len);
+                write(fds[1], buf, str_len);
+            }
+            close(clnt_sock);
+            puts("client disconnected...");
+            return 0;
+        }else{
+            close(clnt_sock);
+        }
+    }
+    close(serv_sock);
     return 0;
 }
+void read_childproc(int sig){
+    pid_t pid;
+    int status;
+    pid = waitpid(-1, &status, WNOHANG);
+    printf("removed proc id: %d \n", pid);
+}
+void error_handling(char *message){
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
+
