6f812e8315b8816e8529fb5ee251127def293c8a
jdfrozen <616769416@qq.com>
2020-12-09 15:28:48 +0800

    去掉空格

--- network/server.c
+++ network/server.c
@@ -1,50 +1,72 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<sys/types.h>
-#include<sys/time.h>
-#include<unistd.h>
 #include<string.h>
+#include<unistd.h>
+#include<arpa/inet.h>
+#include<sys/socket.h>
+#include<sys/time.h>
+#include<sys/select.h>
 
-const int LEN = 1024;
-int fds[2];          //只监测标准输入与输出这两个文件描述符
-int main(int argc, char* argv[]){
-    int std_in = 0;
-    int std_out = 1;
-    int fds_max = 1;
-    fd_set reads, writes;
+#define BUF_SIZE 100
+
+void error_handling(char* message) {
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
+
+int main(int argc, char* argv[]) {
+    int serv_sock, clnt_sock;
+    struct sockaddr_in serv_addr, clnt_addr;
+    char buf[BUF_SIZE];
     struct timeval timeout;
-    fds[0] = std_in;
-    fds[1] = std_out;
-    while(1){
-        FD_ZERO(&reads);
-        FD_ZERO(&writes);
-        FD_SET(std_in, &reads);          //标准输入关注的是读事件
-        FD_SET(std_out, &writes);       //标准输出关注的是写事件
+    fd_set reads, cpy_reads;
+    if (argc != 2) {
+        printf("Usage : %s <port> \n", argv[0]);
+        exit(1);
+    }
+    serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+    if (serv_sock == -1){error_handling("socket() error");}
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(atoi(argv[1]));
+    if (bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1){error_handling("bind() error");}
+    if (listen(serv_sock, 5) == -1){error_handling("listen() error");}
+    FD_ZERO(&reads);
+    FD_SET(serv_sock, &reads);
+    int fd_max = serv_sock;
+    while (1) {
+        cpy_reads = reads;
         timeout.tv_sec = 5;
-        timeout.tv_usec = 0;
-        switch(select(fds_max + 1, &reads, &writes, NULL, &timeout)){
-            case 0:
-                printf("select time out ......\n");
-                break;
-            case -1:
-                perror("select");
-                break;
-            default:
-                if(FD_ISSET(fds[0], &reads)){
-                    char buf[LEN];
-                    memset(buf, '\0', LEN);
-                    fgets(buf);
-                    printf("echo: %s\n", buf);
-                    if(strncmp(buf, "quit", 4) == 0){
-                        exit(0);
+        timeout.tv_usec = 5000;
+        int fd_num;
+        if ((fd_num = select(fd_max + 1, &cpy_reads, 0, 0, &timeout)) == -1){break;}
+        if (fd_num == 0){continue;}
+        for (int i = 0; i < fd_max + 1; i++) {
+            if (FD_ISSET(i, &cpy_reads)) {
+                // 判断是新的链接请求还是读数据的到来
+                if (i == serv_sock){
+                    socklen_t clnt_addr_size = sizeof(clnt_addr);
+                    clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+                    FD_SET(clnt_sock, &reads);
+                    if (fd_max < clnt_sock) {
+                        fd_max = clnt_sock;
+                    }
+                    printf("Connected client: %d \n", clnt_sock);
+                }else {
+                    int str_len = read(i, buf, BUF_SIZE);
+                    if (str_len == 0) {
+                        FD_CLR(i, &reads);
+                        close(i);
+                        printf("closed client: %d \n", i);
+                    }else {
+                        write(i, buf, str_len);
                     }
                 }
-                if(FD_ISSET(fds[1], &writes)){
-                    char* buf = "write is ready.......\n";
-                    printf("%s", buf);
-                    sleep(5);
-                }
-                break;
+            }
         }
     }
+    close(serv_sock);
+    return 0;
 }
