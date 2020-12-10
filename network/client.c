7c9d2710cb82a267313b4bfe0dda83b0f280d83b
jdfrozen <616769416@qq.com>
2020-12-10 17:42:42 +0800

    去掉空格

--- network/client.c
+++ network/client.c
@@ -1,40 +1,50 @@
-/* 基于广播的Sender */
+/* 回声客户端 */
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include<unistd.h>
 #include<arpa/inet.h>
 #include<sys/socket.h>
-#define BUF_SIZE 30
-void error_handling(char *message);
-int main(int argc, char *argv[]){
-	int send_sock;
-	struct sockaddr_in broad_adr;
-	FILE *fp;
-	char buf[BUF_SIZE];
-	if (argc != 3) {
-		printf("Usage: %s <GroupIP> <PORT> \n",argv[0]);
-		exit(1);
-	}
-	send_sock = socket(PF_INET,SOCK_DGRAM,0);
-	memset(&broad_adr,0,sizeof(broad_adr));
-	broad_adr.sin_family = AF_INET;
-	broad_adr.sin_addr.s_addr = inet_addr(argv[1]);
-	broad_adr.sin_port = htons(atoi(argv[2]));
-    int so_brd = 1;
-	setsockopt(send_sock,SOL_SOCKET,SO_BROADCAST,(void*)&so_brd,sizeof(so_brd));
-	if((fp = fopen("news.txt","r")) == NULL){error_handling("fopen() error!");}
-	/* 实际传输数据的区域,基于UDP套接字传输数据,*/
-	while(!feof(fp)){
-		fgets(buf,BUF_SIZE,fp);	//从fp中读取数据,每次读取一行,每次最多读取BUF_SIZE-1个字符,读取的数据保存到buf
-		sendto(send_sock,buf,strlen(buf),0,(struct sockaddr*)&broad_adr,sizeof(broad_adr));		//更改UDP套接字可选项，使其能够发送广播数据
-		sleep(2);				//给传输数据提供一定的时间间隔
-	}
-	close(send_sock);
-	return 0;
-}
+
+#define BUF_SIZE 1024
+
 void error_handling(char *message){
 	fputs(message,stderr);
 	fputc('\n',stderr);
 	exit(1);
+}
+
+int main(int argc,char *argv[]){
+	int sock;
+	char message[BUF_SIZE];
+	int str_len;
+	struct sockaddr_in serv_adr;
+	FILE * readfp;
+	FILE * writefp;
+	if(argc != 3) {
+		printf("Usage : %s <IP> <port>\n",argv[0]);
+		exit(1);
+	}
+	sock = socket(PF_INET,SOCK_STREAM,0);
+	if (sock == -1){error_handling("socket() error!");}
+	memset(&serv_adr,0,sizeof(serv_adr));
+	serv_adr.sin_family = AF_INET;
+	serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
+	serv_adr.sin_port = htons(atoi(argv[2]));
+	if (connect(sock,(struct sockaddr*) &serv_adr,sizeof(serv_adr)) == -1){error_handling("connect() error!");}
+	readfp = fdopen(sock,"r");
+	writefp = fdopen(sock,"w");
+	while (1) {
+		fputs("Input message(Q to quit): ",stdout);
+		fgets(message,BUF_SIZE,stdin);
+		if (!strcmp(message,"q\n") || !strcmp(message,"Q\n"))		// 输入q/Q退出
+			break;
+		fputs(message,writefp);
+		fflush(writefp);
+		fgets(message,BUF_SIZE,readfp);
+		printf("Message from server: %s",message);
+	}
+	fclose(writefp);
+	fclose(readfp);
+	return 0;
 }
--- network/server.c
+++ network/server.c
@@ -1,41 +1,56 @@
-/* 基于广播的Receiver */
+/* 回声服务器端echo_server.c */
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include<unistd.h>
 #include<arpa/inet.h>
 #include<sys/socket.h>
-#define BUF_SIZE 30
 
-void error_handling(char *message);
-int main(int argc,char *argv[]){
-	int recv_sock;
-	int str_len;
-	char buf[BUF_SIZE];
-	struct sockaddr_in adr;
-	struct ip_mreq join_adr;
-	if (argc != 2) {
-		printf("Usage: %s <GroupIP> <PORT> \n",argv[0]);
-		exit(1);
-	}
-	recv_sock = socket(PF_INET,SOCK_DGRAM,0);
-	memset(&adr,0,sizeof(adr));
-	adr.sin_family = AF_INET;
-	adr.sin_addr.s_addr = htonl(INADDR_ANY);
-	adr.sin_port = htons(atoi(argv[1]));
-	if (bind(recv_sock,(struct sockaddr*)&adr,sizeof(adr)) == -1){error_handling("bind() error!");}
-	while(1){
-		str_len = recvfrom(recv_sock,buf,BUF_SIZE-1,0,NULL,0);	//接收数据
-		if (str_len < 0)
-			break;
-		buf[str_len] = 0;
-		fputs(buf,stdout);
-	}
-	close(recv_sock);
-	return 0;
-}
+#define BUF_SIZE 1024
+
 void error_handling(char *message){
 	fputs(message,stderr);
 	fputc('\n',stderr);
 	exit(1);
+}
+int main(int argc,char *argv[]){
+	int serv_sock, clnt_sock;
+	char message[BUF_SIZE];
+	int str_len, i;
+	struct sockaddr_in serv_adr, clnt_adr;
+	socklen_t clnt_adr_sz;
+	FILE * readfp;
+	FILE * writefp;
+	if (argc != 2) {
+		printf("Usage : %s <port> \n",argv[0]);
+		exit(1);
+	}
+	serv_sock = socket(PF_INET,SOCK_STREAM,0);
+	if (serv_sock == -1){error_handling("socket() error!");}
+	memset(&serv_adr,0,sizeof(serv_adr));
+	serv_adr.sin_family = AF_INET;
+	serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_adr.sin_port = htons(atoi(argv[1]));
+	if (bind(serv_sock,(struct sockaddr*) &serv_adr,sizeof(serv_adr)) == -1){error_handling("bind() error!");}
+	if (listen(serv_sock,5) == -1){error_handling("listen() error!");}
+	clnt_adr_sz = sizeof(clnt_adr);
+	for (i = 0; i < 5; i++){
+		clnt_sock = accept(serv_sock,(struct sockaddr*)&clnt_adr,&clnt_adr_sz);
+		if (clnt_sock == -1){
+		    error_handling("accept() error!");
+		}else{
+		    printf("Connected client %d\n",i+1);
+		}
+		readfp  = fdopen(clnt_sock,"r");
+		writefp = fdopen(clnt_sock,"w");
+		while(!feof(readfp)){
+			fgets(message,BUF_SIZE,readfp);
+			fputs(message,writefp);
+			fflush(writefp);			//冲洗缓冲
+		}
+		fclose(readfp);
+		fclose(writefp);
+	}
+	close(serv_sock);
+	return 0;
 }
