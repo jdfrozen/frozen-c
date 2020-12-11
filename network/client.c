71313578c2ef470e85949cd748ec0e8ecbe6126d
jdfrozen <616769416@qq.com>
2020-12-11 18:46:14 +0800

    去掉空格

--- network/client.c
+++ network/client.c
@@ -1,50 +1,79 @@
-/* 回声客户端 */
+/* 聊天程序客户端 */
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include<unistd.h>
 #include<arpa/inet.h>
 #include<sys/socket.h>
-
+#include<pthread.h>
 #define BUF_SIZE 1024
+#define NAME_SIZE 20
 
-void error_handling(char *message){
-	fputs(message,stderr);
-	fputc('\n',stderr);
-	exit(1);
-}
+void * send_msg(void * arg);
+void * recv_msg(void * arg);
+void error_handling(char *message);
+
+char name[NAME_SIZE] = "[DEFAULT]";
+char msg[BUF_SIZE];
 
 int main(int argc,char *argv[]){
 	int sock;
-	char message[BUF_SIZE];
-	int str_len;
 	struct sockaddr_in serv_adr;
-	FILE * readfp;
-	FILE * writefp;
-	if(argc != 3) {
-		printf("Usage : %s <IP> <port>\n",argv[0]);
+	pthread_t snd_thread, rcv_thread;
+	void * thread_return;
+	if(argc != 4) {
+		printf("Usage : %s <IP> <port> <name> \n",argv[0]);
 		exit(1);
 	}
+	sprintf(name,"[%s]", argv[3]);			//第四个参数为客户端名字
 	sock = socket(PF_INET,SOCK_STREAM,0);
 	if (sock == -1){error_handling("socket() error!");}
 	memset(&serv_adr,0,sizeof(serv_adr));
 	serv_adr.sin_family = AF_INET;
 	serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
 	serv_adr.sin_port = htons(atoi(argv[2]));
-	if (connect(sock,(struct sockaddr*) &serv_adr,sizeof(serv_adr)) == -1){error_handling("connect() error!");}
-	readfp = fdopen(sock,"r");
-	writefp = fdopen(sock,"w");
-	while (1) {
-		fputs("Input message(Q to quit): ",stdout);
-		fgets(message,BUF_SIZE,stdin);
-		if (!strcmp(message,"q\n") || !strcmp(message,"Q\n"))		// 输入q/Q退出
-			break;
-		fputs(message,writefp);
-		fflush(writefp);
-		fgets(message,BUF_SIZE,readfp);
-		printf("Message from server: %s",message);
-	}
-	fclose(writefp);
-	fclose(readfp);
+	if (connect(sock,(struct sockaddr*) &serv_adr,sizeof(serv_adr)) == -1){
+		error_handling("connect() error!");
+	}else{
+		puts("Connected.........");
+    }
+	pthread_create(&snd_thread,NULL,send_msg,(void*)&sock);
+	pthread_create(&rcv_thread,NULL,recv_msg,(void*)&sock);
+	pthread_join(snd_thread,&thread_return);	//返回值保存到thread_return
+	pthread_join(rcv_thread,&thread_return);	//返回值保存到thread_return
+	close(sock);
 	return 0;
+}
+//send thread main
+void * send_msg(void * arg){
+	int sock = *((int*)arg);
+	char name_msg[NAME_SIZE+BUF_SIZE];
+	while(1){
+		fgets(msg,BUF_SIZE,stdin);	//读取输入到msg
+		if (!strcmp(msg,"q\n") || !strcmp(msg,"Q\n")){
+			close(sock);
+			exit(0);
+		}
+		sprintf(name_msg,"%s %s",name,msg);	//把名字(命令行参数)和消息写入name_msg数组
+		write(sock,name_msg,strlen(name_msg));
+	}
+	return NULL;
+}
+//read thread main
+void * recv_msg(void * arg)		{
+	int sock= *((int*)arg);
+	char name_msg[NAME_SIZE+BUF_SIZE];
+	int str_len;
+	while(1){
+		str_len = read(sock,name_msg,NAME_SIZE+BUF_SIZE-1);
+		if (str_len == -1){return (void*)-1;}
+		name_msg[str_len] = 0;
+		fputs(name_msg,stdout);
+	}
+	return NULL;
+}
+void error_handling(char *msg){
+	fputs(msg,stderr);
+	fputc('\n',stderr);
+	exit(1);
 }
--- network/server.c
+++ network/server.c
@@ -1,85 +1,84 @@
+/* 聊天服务器端chat_server.c */
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include<unistd.h>
 #include<arpa/inet.h>
 #include<sys/socket.h>
-#include<sys/epoll.h>
+#include<pthread.h>
+#include<semaphore.h>
+#define BUF_SIZE 100
+#define MAX_CLNT 256
 
-#define BUF_SIZE 4
-#define EPOLL_SIZE 50
 void error_handling(char *message);
+void * handle_clnt(void * arg);
+void send_msg(char * msg,int len);
 
-int main(int argc, char *argv[]){
+int clnt_cnt = 0;           //接入的客户端套接字的数量
+int clnt_socks[MAX_CLNT];	//管理接入的客户端套接字的数组
+pthread_mutex_t mutx;
+
+int main(int argc,char *argv[]){
 	int serv_sock, clnt_sock;
 	struct sockaddr_in serv_adr, clnt_adr;
-	socklen_t adr_sz;
-	int str_len, i;
-	char buf[BUF_SIZE];
-	struct epoll_event *ep_events;
-	struct epoll_event event;
-	int epfd, event_cnt;
-	if (argc != 2){
-		printf("Usage: %s <port> \n",argv[0]);
+	socklen_t clnt_adr_sz;
+	pthread_t t_id;
+	if (argc != 2) {
+		printf("Usage : %s <port> \n",argv[0]);
 		exit(1);
 	}
+	pthread_mutex_init(&mutx,NULL);
 	serv_sock = socket(PF_INET,SOCK_STREAM,0);
+	if (serv_sock == -1){error_handling("socket() error!");}
+
 	memset(&serv_adr,0,sizeof(serv_adr));
 	serv_adr.sin_family = AF_INET;
 	serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
 	serv_adr.sin_port = htons(atoi(argv[1]));
-	if (bind(serv_sock, (struct sockaddr*) &serv_adr, sizeof(serv_adr)) == -1){error_handling("bind() error!");}
+	if (bind(serv_sock,(struct sockaddr*) &serv_adr,sizeof(serv_adr)) == -1){error_handling("bind() error!");}
 	if (listen(serv_sock,5) == -1){error_handling("listen() error!");}
-	epfd = epoll_create(EPOLL_SIZE);		//创建epoll例程文件描述符
-	ep_events = malloc(sizeof(struct epoll_event) * EPOLL_SIZE);
-	event.events = EPOLLIN;
-	event.data.fd = serv_sock;
-	epoll_ctl(epfd, EPOLL_CTL_ADD, serv_sock, &event);	//在epfd内注册监视serv__sock,监视其读取数据的情况
-	while(1)
-	{
-		event_cnt = epoll_wait(epfd, ep_events, EPOLL_SIZE, -1);	//成功返回发生事件的文件描述符数。失败返回-1,发生事件的文件描述符保存到events所指的结构体中
-		if (event_cnt == -1){
-			puts("epoll_wait() error!");
-			break;
-		}
-		puts("return epoll_wait");			//验证epoll_wait调用次数
-		for (i = 0; i < event_cnt; i++){
-			if (ep_events[i].data.fd == serv_sock){
-			    //服务器端套接字接收连接
-				adr_sz = sizeof(clnt_adr);
-				clnt_sock = accept(serv_sock,(struct sockaddr*)&clnt_adr,&adr_sz);
-				setnonblockingmode(clnt_sock);			//改为非阻塞
-				event.events = EPOLLIN | EPOLLET;       //套接字事件注册方式改为边缘触发
-				event.data.fd = clnt_sock;
-				printf("connected client: %d \n",clnt_sock);
-			}else{
-			    //连接客户端套接字读取数据
-				while(1){
-                    //边缘触发发生事件时，需要读取输入缓冲中的所有数据，因此需要循环调用read函数
-                    str_len = read(ep_events[i].data.fd, buf, BUF_SIZE);
-                    if (str_len == 0){
-                        epoll_ctl(epfd,EPOLL_CTL_DEL,ep_events[i].data.fd,NULL);
-                        close(ep_events[i].data.fd);
-                        printf("closed client: %d \n",ep_events[i].data.fd);
-                        break;
-                    }else if(str_len < 0) {
-                        if (errno == EAGAIN)
-                            break;
-                    }else{
-                        write(ep_events[i].data.fd, buf, str_len);	//echo!
-                    }
-                }
-			}
-		}
+	while(1){
+		clnt_adr_sz = sizeof(clnt_adr);
+		clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_adr,&clnt_adr_sz);
+
+		pthread_mutex_lock(&mutx);
+		clnt_socks[clnt_cnt++] = clnt_sock;		//每当有新连接，将连接的套接字写入变量clnt_cnt和clnt_socks
+		pthread_mutex_unlock(&mutx);
+
+		pthread_create(&t_id,NULL,handle_clnt,(void*)&clnt_sock);	//创建线程向接入的客户端提供服务
+		pthread_detach(t_id);		//从内存中完全销毁已终止的线程
+		printf("Connected client IP: %s \n",inet_ntoa(clnt_adr.sin_addr));
 	}
 	close(serv_sock);
-	close(epfd);
 	return 0;
 }
 
-void setnonblockingmode(int fd){
-	int flag = fcntl(fd,F_GETFL,0);
-	fcntl(fd,F_SETFL,flag|O_NONBLOCK);
+void * handle_clnt(void * arg){
+	int clnt_sock = *((int*)arg);
+	int str_len = 0, i;
+	char msg[BUF_SIZE];
+	while((str_len = read(clnt_sock,msg,sizeof(msg))) != 0){send_msg(msg,str_len);}
+	pthread_mutex_lock(&mutx);
+	for (i=0; i<clnt_cnt; i++){
+	    //remove disconnected client
+		if (clnt_sock == clnt_socks[i]){
+			while(i++ < clnt_cnt-1)
+				clnt_socks[i] = clnt_socks[i+1];
+			break;
+		}
+	}
+	clnt_cnt--;
+	pthread_mutex_unlock(&mutx);
+	close(clnt_sock);
+	return NULL;
+}
+//send to all：向所有客户端发送消息
+void send_msg(char * msg,int len){
+	int i;
+	pthread_mutex_lock(&mutx);
+	for ( i=0; i<clnt_cnt; i++)
+		write(clnt_socks[i], msg, len);
+	pthread_mutex_unlock(&mutx);
 }
 void error_handling(char *message){
 	fputs(message,stderr);
