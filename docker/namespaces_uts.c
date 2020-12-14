9406390a8185cbb570dd28ad453519f8cdd049c4
jdfrozen <616769416@qq.com>
2020-12-14 20:02:09 +0800

    去掉空格

--- docker/namespaces_uts.c
+++ docker/namespaces_uts.c
@@ -16,10 +16,8 @@ char* const container_args[] = {
 };
 int container_main(void* arg)
 {
-    printf("Container [%5d] - inside the container!\n", getpid());
-    sethostname("container",10);
-    /* 重新mount proc文件系统到 /proc下 */
-    system("mount -t proc proc /proc");
+    printf("Container - inside the container!\n");
+    sethostname("container",10); /* 设置hostname */
     execv(container_args[0], container_args);
     printf("Something's wrong!\n");
     return 1;
@@ -27,10 +25,9 @@ int container_main(void* arg)
 
 int main()
 {
-    printf("Parent [%5d] - start a container!\n", getpid());
-    /* 启用Mount Namespace - 增加CLONE_NEWNS参数 */
+    printf("Parent - start a container!\n");
     int container_pid = clone(container_main, container_stack+STACK_SIZE,
-            CLONE_NEWUTS | CLONE_NEWPID | CLONE_NEWNS | SIGCHLD, NULL);
+            CLONE_NEWUTS | SIGCHLD, NULL); /*启用CLONE_NEWUTS Namespace隔离 */
     waitpid(container_pid, NULL, 0);
     printf("Parent - container stopped!\n");
     return 0;
